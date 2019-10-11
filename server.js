6615458ae0a683b6cf712426e0a2a19536784457
dsanderson90 <dsanderson3@cnm.edu>
2019-10-11 14:21:50 -0600

    added cors

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -9,6 +9,7 @@ const Trip = require('./Trip')
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
